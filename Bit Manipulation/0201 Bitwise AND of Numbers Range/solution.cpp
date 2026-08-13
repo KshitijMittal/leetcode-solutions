3499c195f2d33fadfb66dee2207ed0418a675938
KshitijMittal <193310873+KshitijMittal@users.noreply.github.com>
2026-08-13 06:57:11 +0000

    Auto-sync LeetCode solutions

--- Bit Manipulation/0201 Bitwise AND of Numbers Range/solution.cpp
+++ Bit Manipulation/0201 Bitwise AND of Numbers Range/solution.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        
+        // if (left >= INT_MAX || right >= INT_MAX) return 0;
+
+        // int ans = left;
+        // for (long long i = left; i <= right; i++) {
+        //     ans &= i;
+        //     if (ans == 0) break;
+        // }
+
+        // return ans;
+
         int count = 0;
-        while (left != right) {
+        while (left < right) {
             left >>= 1;
             right >>= 1;
-            count++;
+            count ++;
         }
 
-        return (left << count);
+        return left << count;
     }
 
 };
